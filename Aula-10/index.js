6c18202436d6c362ee9071ee54311017c6b1bbf2
Cledimilson batista dos santos <60282188+cledimillson@users.noreply.github.com>
2023-05-24 22:40:52 -0300

    criei a aula 10

---
+++ Aula-10/index.js
@@ -0,0 +1,12 @@
+let nome = "Santos"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+// windows.alert(nome)
+//nome = window.prompt("Insira seu nome?")
+
+//let confirme = window.confirm("Confirme essa ação?")
+
+alert("Te")
+confirm("Confirme essa msg")
---
+++ Aula-18/index.js
@@ -0,0 +1,8 @@
+const idade = prompt("Informe sua idade: ")
+
+if (idade >= 18) {
+    alert("Você e maior de idade")
+
+} else {
+    alert("Você e menor de idade.")
+}
