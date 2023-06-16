50c3283b1dd83447aeeb8d3a2014b47001b70d95
Cledimilson batista dos santos <60282188+cledimillson@users.noreply.github.com>
2023-06-15 22:52:19 -0300

    desafio

---
+++ Modulo 2/Aula-8_Exerc-11/js/script.js
@@ -0,0 +1,7 @@
+function addPlayer() {
+    const position = document.getElementById('position').value;
+    const name = document.getElementById('name').value;
+    const number = document.getElementById('number').value;
+
+    console.log({position, name, number});
+}
