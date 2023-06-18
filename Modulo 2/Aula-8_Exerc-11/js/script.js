6367bf2049a21ba03f29e7b9715ed00918703119
Cledimilson batista dos santos <60282188+cledimillson@users.noreply.github.com>
2023-06-17 22:59:45 -0300

    aula 10

--- Modulo 2/Aula-8_Exerc-11/js/script.js
+++ Modulo 2/Aula-8_Exerc-11/js/script.js
@@ -3,5 +3,31 @@ function addPlayer() {
     const name = document.getElementById('name').value;
     const number = document.getElementById('number').value;
 
-    console.log({position, name, number});
+    const confirmation = confirm("Escalar " + name + " como " + position + "?");
+
+    if (confirmation) {
+        const teamList = document.getElementById('teamList');
+        const playerItem = document.createElement('li');
+        playerItem.id = 'player-' + number
+        playerItem.innerText = position + ": " + name + "(" + number + ")";
+        teamList.appendChild(playerItem);
+
+        document.getElementById('position').value = '';
+        document.getElementById('name').value = '';
+        document.getElementById('number').value = '';
+    }
+
+}
+
+function removePlayer() {
+    const mumber = document.getElementById('numberToRemove').value;
+    const playerToRemove = document.getElementById('player-' + number);
+
+    const confirmation = confirm('Remover o jogador ' + playerToRemove.innerText + '?');
+
+    if (confirmation) {
+        //playerToRemove.remove();
+        document.getElementById('teamList').removeChild(playerToRemove);
+        document.getElementById('numberToRemove').value = "";
+    }
 }
